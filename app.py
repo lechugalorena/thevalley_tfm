f19c94dfb3c58627ed577654a48edf2dfaebfa87
lechugalorena <llechugacuesta@gmail.com>
2025-01-26 19:42:13 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -19,16 +19,30 @@ Introduce los valores de las variables y obten la probabilidad de que se produzc
 hotel_grande = st.selectbox("¿Es un hotel grande?", options=["No", "Sí"])
 hotel_grande_dummy = 1 if hotel_grande == "Sí" else 0
 
-lead_time = st.number_input("Lead Time (días de antelación)", min_value=0, value=800)
-noches = st.number_input("Número de noches", min_value=1, value=4)
+lead_time = st.number_input("Lead Time (numero de dias desde la reserva hasta el check-in)", min_value=0, value=800)
+noches = st.slider("Número de noches reservadas", min_value=1, max_value=1100, value=4)
 familia = st.selectbox("¿Es una familia?", options=["No", "Sí"])
 familia_dummy = 1 if familia == "Sí" else 0
-valor_reserva = st.number_input("Valor de la reserva (€)", min_value=0, value=3000)
-cunas = st.number_input("Número de cunas", min_value=0, value=1)
-adultos = st.number_input("Número de adultos", min_value=1, value=4)
+valor_reserva = st.number_input("Valor de la reserva (€)", min_value=0, value=120000)
+cunas = st.selectbox("¿Se ha solicitado una cuna?", options=["No", "Sí"])
+cunas_dummy = 1 if cunas == "Sí" else 0
+adultos = st.slider("Número de adultos", min_value=1, max_value=8, value=2)
 fidelidad = st.selectbox("¿Es un cliente fidelizado?", options=["No", "Sí"])
 fidelidad_dummy = 1 if fidelidad == "Sí" else 0
-aux_tipo = st.number_input("Tipo auxiliar", min_value=0, value=6)
+aux_tipo = st.selectbox(
+    "Tipo de habitación",
+    options=[
+        "Default Category (0)",
+        "Deluxe (1)",
+        "Junior Suite (2)",
+        "Presidential (3)",
+        "Standard (4)",
+        "Suite (5)",
+        "Superior (6)"
+    ]
+)
+aux_tipo_valor = int(aux_tipo.split("(")[1].strip(")"))
+
 
 # Botón para predecir
 if st.button("Predecir"):
