7be31b6be546689794093560d52cefd24327d918
lechugalorena <llechugacuesta@gmail.com>
2025-02-04 20:42:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,33 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# Configuración del servidor y credenciales
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+GMAIL_USER = "thevalleypalladium@gmail.com"
+GMAIL_PASSWORD = "poiu vjjs upgl brme"
+
+# Mensaje HTML con la acción de descuento
+mensaje_html = '''
+<html>
+    <head></head>
+    <body>
+        <h1>Oferta Exclusiva para Palladium</h1>
+        <p>Estimado/a cliente,</p>
+        <p>Queremos evitar la cancelación y, para ello, te ofrecemos un <strong>descuento especial</strong> en tu próxima reserva.</p>
+        <p>Haz clic en el siguiente enlace para activar tu descuento:</p>
+        <p><a href="https://palladiumhotels.com/activar-descuento" style="padding:10px 20px; color: white; background-color: #007BFF; text-decoration: none;">Activar Descuento</a></p>
+        <p>¡No dejes pasar esta oportunidad!</p>
+        <p>Saludos,</p>
+        <p>Equipo Palladium</p>
+    </body>
+</html>
+'''
+ 
 
 # Carga del modelo entrenado
 with open('modelo.pkl', 'rb') as file:
@@ -61,9 +88,41 @@ if st.button("Predecir"):
         'FIDELIDAD_DUMMY': [fidelidad_dummy],
         'AUX_TIPO': [aux_tipo_valor]
     })
-    
+
     # Realizar la predicción
     probabilidad = logmodel.predict_proba(data)[:, 1]
-    
+
     # Mostrar el resultado
     st.success(f"La probabilidad de cancelación es del {probabilidad[0] * 100:.2f}%")
+
+    if probabilidad[0] * 100 > 50:
+              # Input fields
+        recipient_email = st.text_input("Destinatario:", "")
+        
+        # Send button
+        if st.button("Send Email"):
+            if recipient_email:
+                asunto = "Oferta especial para Palladium: Activa tu descuento"
+                
+                # Crear el mensaje MIME y adjuntar el contenido HTML
+                msg = MIMEMultipart("alternative")
+                msg["From"] = GMAIL_USER
+                msg["To"] = recipient_email
+                msg["Subject"] = asunto
+                msg.attach(MIMEText(mensaje_html, "html"))
+                
+                try:
+                        # Conectar al servidor SMTP de Gmail
+                        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+                        server.starttls()  # Iniciar conexión segura
+                        server.login(GMAIL_USER, GMAIL_PASSWORD)
+                        server.sendmail(GMAIL_USER, destinatario, msg.as_string())
+                        server.quit()
+                        print("Correo enviado correctamente.")
+                except Exception as e:
+                        print(f"Error al enviar el correo: {e}")
+                st.success(result)
+            else:
+                st.warning("⚠️ Please fill in all fields before sending.")
+
+
